bbb40c2acac8d9c8cf1b8d1f25cb7886b5ca6317
Raphael Guarilha <902478+guarilha@users.noreply.github.com>
2025-02-17 22:50:33 -0300

    finish documenting the public functions

--- api_account_management.go
+++ api_account_management.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// GetAccountBalances retrieves the account balances from the API.
+// Returns an AccountBalances object containing balance information or an error if the request fails.
 func (c *Client) GetAccountBalances() (*AccountBalances, error) {
 	var result AccountBalances
 	err := c.doRequest(http.MethodGet, "/api/v1/account-balances", nil, &result)
@@ -16,6 +18,8 @@ func (c *Client) GetAccountBalances() (*AccountBalances, error) {
 	return &result, nil
 }
 
+// GetSafeConfig retrieves the Safe wallet configuration from the API.
+// Returns a SafeConfig object containing wallet settings or an error if the request fails.
 func (c *Client) GetSafeConfig() (*SafeConfig, error) {
 	var result SafeConfig
 	err := c.doRequest(http.MethodGet, "/api/v1/safe-config", nil, &result)
@@ -25,6 +29,8 @@ func (c *Client) GetSafeConfig() (*SafeConfig, error) {
 	return &result, nil
 }
 
+// GetDelayedTransactions retrieves a list of delayed transactions from the API.
+// Returns an array of DelayTransaction objects or an error if the request fails.
 func (c *Client) GetDelayedTransactions() (*[]DelayTransaction, error) {
 	var result []DelayTransaction
 	err := c.doRequest(http.MethodGet, "/api/v1/delay-relay", nil, &result)
@@ -34,6 +40,8 @@ func (c *Client) GetDelayedTransactions() (*[]DelayTransaction, error) {
 	return &result, nil
 }
 
+// GetEoaAccounts retrieves a list of externally owned accounts (EOAs) from the API.
+// Returns an array of EoaAccount objects or an error if the request fails.
 func (c *Client) GetEoaAccounts() (*[]EoaAccount, error) {
 	var result struct {
 		Data struct {
@@ -47,6 +55,11 @@ func (c *Client) GetEoaAccounts() (*[]EoaAccount, error) {
 	return &result.Data.EoaAccounts, nil
 }
 
+// CreateEoa creates a new externally owned account (EOA) with the specified Ethereum address.
+// Parameters:
+//   - address: Ethereum address to associate with the new EOA
+//
+// Returns the created EoaAccount object or an error if the creation fails.
 func (c *Client) CreateEoa(address common.Address) (*EoaAccount, error) {
 	var result EoaAccount
 	err := c.doRequest(http.MethodPost, "/api/v1/eoa-accounts", struct{ address string }{address.String()}, &result)
@@ -56,6 +69,11 @@ func (c *Client) CreateEoa(address common.Address) (*EoaAccount, error) {
 	return &result, nil
 }
 
+// DeleteEoa removes an externally owned account (EOA) with the specified ID.
+// Parameters:
+//   - id: Unique identifier of the EOA to delete
+//
+// Returns true if deletion was successful, false otherwise, and any error that occurred.
 func (c *Client) DeleteEoa(id string) (bool, error) {
 	err := c.doRequest(http.MethodPost, fmt.Sprintf("/api/v1/eoa-accounts/%s", id), nil, nil)
 	if err != nil {
--- wallet/sign.go
+++ wallet/sign.go
@@ -8,15 +8,26 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+// signHash creates an Ethereum-specific hash of the given data by prefixing it with
+// the Ethereum Signed Message header and computing the Keccak256 hash.
+// This follows EIP-191 for signing and verifying Ethereum messages.
 func signHash(data []byte) common.Hash {
 	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
 	return crypto.Keccak256Hash([]byte(msg))
 }
 
+// SignMessage signs a string message using the provided private key.
+// It converts the string to bytes and uses SignBytes internally.
+// Returns the signature as a byte array or an error if signing fails.
 func SignMessage(message string, privateKey *ecdsa.PrivateKey) ([]byte, error) {
 	return SignBytes([]byte(message), privateKey)
 }
 
+// SignBytes signs a byte array message using the provided private key.
+// It first creates an Ethereum-specific hash of the message using signHash,
+// then signs it using the private key. The signature's V value is adjusted by adding 27
+// to comply with Ethereum's signature format.
+// Returns the signature as a byte array or an error if signing fails.
 func SignBytes(message []byte, privateKey *ecdsa.PrivateKey) ([]byte, error) {
 	sign := signHash(message)
 
@@ -29,10 +40,16 @@ func SignBytes(message []byte, privateKey *ecdsa.PrivateKey) ([]byte, error) {
 	return signature, nil
 }
 
+// SignatureToString converts a signature byte array to a hexadecimal string
+// prefixed with "0x".
 func SignatureToString(signature []byte) string {
 	return "0x" + common.Bytes2Hex(signature)
 }
 
+// SignRawBytes signs a raw byte array using the provided private key without
+// applying the Ethereum-specific message prefix. The signature's V value is adjusted
+// by adding 27 to comply with Ethereum's signature format.
+// Returns the signature as a byte array or an error if signing fails.
 func SignRawBytes(message []byte, privateKey *ecdsa.PrivateKey) ([]byte, error) {
 	signature, err := crypto.Sign(message, privateKey)
 	if err != nil {
